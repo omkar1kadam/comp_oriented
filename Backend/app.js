b70f9ac045b549ab359b50854deae19c572228e3
omkar1kadam <omkarakadam2408@gmail.com>
2025-08-06 18:59:53 +0530

    try6

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get('/',(req, res) => {
-  res.send('Hello World!  try 5');
+  res.send('Hello World!  try 6');
 }); 
 
 app.use('/users', userRoutes);
