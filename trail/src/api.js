168b57dc0029cbf8992339e3fca93c35533ff47a
omkar1kadam <omkarakadam2408@gmail.com>
2025-08-06 19:21:33 +0530

    Temp rename to force Git case sensitivity 1

--- trail/src/api.js
+++ trail/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://comp-oriented.onrender.com/', // your Node backend port
+  baseURL: 'https://comp-oriented.onrender.com', // your Node backend port
   withCredentials: true,            // send cookies across requests
 });
 
